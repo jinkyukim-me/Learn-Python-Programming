a0f6de9c45c37978cc798f455dff82974d1fc9d0
jinkyu <jinkyukim.dev@gmail.com>
2020-01-24 02:40:18 +0900

    Modify ex2.py

--- exercises/ex2.py
+++ exercises/ex2.py
@@ -1,1 +1,9 @@
-# A comment, this is so
+# A comment, this is so you read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and the comment out code:
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
