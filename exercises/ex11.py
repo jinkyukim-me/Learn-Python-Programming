2ad6d7dae91aa3236df0209f95fc9f01972643b6
Jinkyu Kim <jinkyukim.dev@gmail.com>
2020-01-28 01:03:16 +0900

    Add files
    ex10.py, ex11.py, ex12.py, ex13.py, ex15.py, ex15_sample.txt

---
+++ exercises/ex11.py
@@ -0,0 +1,13 @@
+# Excercise 11. Asking Questions
+
+print("How old are you?", end=' ')
+age = input()
+
+print("How tall are you?(cm)", end=' ')
+height = input()
+
+print("How much do you weight?(kg)", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
