36686cc770b98db15c0b2b8bf95b168d571ea53e
Jinkyu Kim <jinkyukim.dev@gmail.com>
2020-01-27 17:05:16 +0900

    Update file
    ex10.py

---
+++ exercises/ex10.py
@@ -0,0 +1,17 @@
+# Excercise 10. What was That?
+
+tabby_cat = "\tI'm tabbed in."
+person_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(person_cat)
+print(backslash_cat)
+print(fat_cat)
