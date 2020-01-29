4536d39f0797256b87061d8a046230d28d8cd3f0
Jinkyu Kim <jinkyukim.dev@gmail.com>
2020-01-30 01:19:30 +0900

    Add files

---
+++ exercises/ex27.py
@@ -0,0 +1,25 @@
+# Exercise 27. Memorizing Logic
+
+print("""
+        NOT
+        not False -> True
+        not True -> False
+        
+        OR
+        True or False -> True
+        True or True -> True
+        False or True -> True
+        False or False -> False
+
+        AND
+        True or False -> False
+        True or True -> True
+        False or True -> False
+        False or False -> False
+
+        != (not equal)
+        == (equal)
+        >= (greater-than-equal)
+        <= (less-than-equal)
+        """)
+
---
+++ exercises/ex28.py
@@ -0,0 +1,13 @@
+# Exercise 28. Boolean Practice
+
+print("True and True : ",True and True)
+print("False and True : ", False and True)
+print("1 == 1 and 2 == 1 : ", 1  == 1 and 2 == 1)
+print('"test" == "test" : ', "test" == "test")
+print("1 == 1 or 2 != 1 : ", 1 == 1 or 2 != 1)
+print("True and 1 == 1 : ", True and 1 == 1)
+print("False and 0 != 0 : ", False and 0 != 0)
+print("True or 1 == 1 : ", True or 1 == 1)
+print('"test" == "testing" : ', "test" == "testing")
+print("1 != 0 and 2 == 1 : ", 1 != 0 and 2 == 1)
+
