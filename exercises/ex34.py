bb23a9c9f704117f255812b4b6a6357d3ecc86bc
Jinkyu Kim <jinkyukim.dev@gmail.com>
2020-01-31 22:26:42 +0900

    Add files

---
+++ exercises/ex34.py
@@ -0,0 +1,8 @@
+# Exercise 34. Accessing Elements of Lists
+
+animals = ['bear', 'python3.6', 'peocock', 'kangaroo', 'whale', 'platypus']
+print(animals)
+print(animals[0])
+print(animals[1])
+print(animals[-1])
+
