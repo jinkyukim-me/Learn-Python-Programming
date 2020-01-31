ad574ca30d27eb394d4a504b57a2958da4fd7391
Jinkyu Kim <jinkyukim.dev@gmail.com>
2020-02-01 01:53:02 +0900

    Add file

---
+++ exercises-extended/variables_print.py
@@ -0,0 +1,33 @@
+print("-" * 40)
+print("""
+    print(*object, sep='', end='/n')
+""")
+print("-" * 40)
+
+# print
+print(3 + 4)
+4 * 5       # ignore
+a = 1
+b = 2
+print(a + b)
+
+# printsep
+f = "Florida"
+n = "New York"
+o = "Oregon"
+c = "California"
+m = "Michigan"
+print(f, n, o, c, m, sep=' ** ')
+
+# printtwo
+a = "Dog"
+b = "Cat"
+print(a)
+print(b)
+
+# printend
+a = "Dog"
+b = "Cat"
+print(a, end='')
+print(b)
+
