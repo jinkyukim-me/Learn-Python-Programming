bdf3591e5fa5bdeeeca1157916a66dc22ec2e814
Jinkyu Kim <jinkyukim.dev@gmail.com>
2020-02-05 00:09:20 +0900

    Modify ex44a.py

--- exercises/ex44a.py
+++ exercises/ex44a.py
@@ -1,14 +1,31 @@
 class Parent(object):
     
+    def __init__(self, blood, lastname):
+        self.blood = blood
+        self.lastname = lastname
+
     def implicit(self):
         print("PARENT implicit()")
 
-class Child(Parent):
-    pass
 
-dad = Parent()
-son = Child()
+class Child(Parent):
+
+    def __init__(self, blood, lastname, gender):
+        super().__init__(blood, lastname)
+        self.gender = gender
+
+    def inheritance(self):
+        print(f"""
+        blood type : {self.blood}
+        last name : {self.lastname}
+        gender : {self.gender}
+        """)
+
+dad = Parent("A", "Kim")
+son = Child("A", "Kim", "Male")
 
 dad.implicit()
 son.implicit()
+
+son.inheritance()
 
