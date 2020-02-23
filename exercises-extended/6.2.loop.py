a0c92f870c43d21e2619f3762f729e68ec2799aa
Jinkyu Kim <jinkyukim.dev@gmail.com>
2020-02-23 15:49:39 +0900

    Add file

--- exercises-extended/6.2.loop.py
+++ exercises-extended/6.2.loop.py
@@ -6,4 +6,15 @@ for dan in range(2, 10):
     for hang in range(2, 10):
         print(dan, '*', hang, '=', dan * hang)
     print()
-    
+
+# gugudanwhile
+dan = 2
+while dan <= 9:
+    hang = 2
+    print(dan, '단')
+    while hang <= 9:
+        print(dan, "*", hang, '=', dan * hang)
+        hang += 1
+    dan += 1
+    print()
+
